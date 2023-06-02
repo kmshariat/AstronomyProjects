f21eab896186b17325a9b0758b6ba02322f35ed8
K.M Shariat Ullah <cast.shariat@gmail.com>
2023-06-02 13:10:24 +0600

    hms2dec function creation

--- crossMatching/crossMatcher.py
+++ crossMatching/crossMatcher.py
@@ -1,9 +1,21 @@
-def hms2deg(hr, m, s):
-    if hr>=0:
-        return (hr+m/60+s/3600)
+#this function converts hour minute second format to decimal degree format
+def hms2dec(hr, m, s):
+    if hr>=0 and hr<=24:
+        return 15*(hr+m/60+s/3600)
     elif hr<0:
-        return -(-hr+m/60+s/3600)
-    
+        return -15*(-hr+m/60+s/3600)
+
+#this function converts hour minute second format to decimal degree format
+def dms2dec(d, m, s):
+    if d>=0:
+        return (d+m/60+s/3600)
+    elif d<0:
+        return -(-d+m/60+s/3600)
+
+#testing
 if __name__=='__main__':
-    hms2deg(73,21,14.4)
-    hms2deg(-5,31,12)
+    print(hms2dec(23,12,6))
+    print(dms2dec(73,21,14.4))
+    print(dms2dec(-5,31,12))
+    print(dms2dec(22, 57, 18))
+    print(dms2dec(-66, 5, 5.1))
