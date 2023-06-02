a7f054cd18b38a96c22554abfd3e039381e04cc3
K.M Shariat Ullah <cast.shariat@gmail.com>
2023-06-02 13:35:44 +0600

    Haversine formula

--- crossMatching/crossMatcher.py
+++ crossMatching/crossMatcher.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #this function converts hour minute second format to decimal degree format
 def hms2dec(hr, m, s):
     if hr>=0 and hr<=24:
@@ -12,10 +14,28 @@ def dms2dec(d, m, s):
     elif d<0:
         return -(-d+m/60+s/3600)
 
-#testing
-if __name__=='__main__':
-    print(hms2dec(23,12,6))
-    print(dms2dec(73,21,14.4))
-    print(dms2dec(-5,31,12))
-    print(dms2dec(22, 57, 18))
-    print(dms2dec(-66, 5, 5.1))
+"""
+this function measures the angular distance between two celestial objects
+
+parameters: 
+right ascension of object 1, declination of object 1
+right ascension of object 2, declination of object 2 
+
+inputs are taken in degrees and output in degrees
+
+This is also called the Haversine formula
+"""
+
+def angular_dist(ra1, dec1, ra2, dec2):
+    ra1 = np.radians(ra1)
+    ra2 = np.radians(ra2)
+    dec1 = np.radians(dec1)
+    dec2 = np.radians(dec2)
+    
+    a = np.sin(np.abs(dec1 - dec2)/2)**2
+    b = np.cos(dec1)*np.cos(dec2)*np.sin(np.abs(ra1 - ra2)/2)**2
+    return 2*np.arcsin(np.sqrt(a + b))*180/np.pi
+
+if __name__ == '__main__':
+  print(angular_dist(21.07, 0.1, 21.15, 8.2))
+  print(angular_dist(10.3, -3, 24.3, -29))
